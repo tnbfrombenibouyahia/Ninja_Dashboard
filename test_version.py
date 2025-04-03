f5a5c810e25627d4181e37f9f88bc4892c2b33f9
tnbfrombenibouyahia <theonaimbenhellalpro@gmail.com>
2025-04-03 17:12:48 +0900

    Derniers ajustements avant déploiement

--- test_version.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-try:
-    import streamlit_authenticator as stauth
-    st.write("✅ Version de streamlit-authenticator :", stauth.__version__)
-except ImportError:
-    st.error("❌ streamlit-authenticator n'est pas installé.")
